33ed51c890729e05add4793e6b3f63acb50800bd
mimi0908 <emilygarciamontes@gmail.com>
2024-04-09 10:06:36 -0500

    se me olvido cambiar de rama asi que subire lo que tengo ahi y ya seguire con dev01

--- laPasteleria/src/utils/BaseDatos.java
+++ laPasteleria/src/utils/BaseDatos.java
@@ -15,7 +15,7 @@ public class BaseDatos {
         
         String hostname="localhost";
         String puerto="3306";
-        String databasename="app_java";
+        String databasename="la_pasteleria";
         String user="root";
         String password="";
         
@@ -32,17 +32,17 @@ public class BaseDatos {
         
         
     }
-    public void imprimirDatos(DefaultTableModel modelo){
+    public void imprimirDatosPersona(DefaultTableModel modelo){
         ResultSet registros = null;
         modelo.setRowCount(0);
         int i=1;
 	try {
-	    String consulta = "SELECT * FROM personas";
+	    String consulta = "SELECT * FROM persona";
 	    registros = manipularDB.executeQuery(consulta);
 	    registros.next();
 	    if(registros.getRow()==1){
 	        do{
-                    Object datos1 []= new Object[]{i, registros.getString("cedula"), registros.getString("nombres")+" "+registros.getNString("Apellidos"), registros.getString("telefono"), registros.getString("email")};
+                    Object datos1 []= new Object[]{i, registros.getString("ID_persona"), registros.getString("Nombre")+" "+registros.getNString("Apellido"), registros.getString("Telefono"), registros.getString("Email")};
                     modelo.addRow(datos1);
                     i++;
 	        }while(registros.next());
@@ -54,17 +54,60 @@ public class BaseDatos {
 	}   
     }
     
-    public void insertarDatos(String documento, String nombre, String apellido, String tel, String correo){
+    public void imprimirDatosEncargo(DefaultTableModel modelo){
+        ResultSet registros = null;
+        modelo.setRowCount(0);
+        int i=1;
+	try {
+	    String consulta = "SELECT * FROM encargo";
+	    registros = manipularDB.executeQuery(consulta);
+	    registros.next();
+	    if(registros.getRow()==1){
+	        do{
+                    Object datos1 []= new Object[]{i, registros.getString("Id_encargo"), registros.getString("FechaPedido")+" "+registros.getNString("FechaEntrega"), registros.getString("Descripcion"), registros.getString("Precio"), registros.getString("Abono"), registros.getString("SaldoPendiente"), registros.getString("Estado"), registros.getString("persona_Id"), registros.getString("id_usuario")};
+                    modelo.addRow(datos1);
+                    i++;
+	        }while(registros.next());
+	    }else{
+	        System.out.println("No se encuentran clientes registrados.");
+	    }
+	} catch (SQLException ex) {
+	    System.out.println("Error al buscar el cliente: "+ex.getMessage());
+	}   
+    }
+    
+    public void imprimirDatosReservas(DefaultTableModel modelo){
+        ResultSet registros = null;
+        modelo.setRowCount(0);
+        int i=1;
+	try {
+	    String consulta = "SELECT * FROM reserva";
+	    registros = manipularDB.executeQuery(consulta);
+	    registros.next();
+	    if(registros.getRow()==1){
+	        do{
+                    Object datos1 []= new Object[]{i, registros.getString("Id_encargo"), registros.getString("persona_id")+" "+registros.getNString("FechaReserva"), registros.getString("Zona"), registros.getString("horaReserva"), registros.getString("Anexos"), registros.getString("Precio"), registros.getString("Abono"), registros.getString("SaldoPendiente"), registros.getString("id_usuario")};
+                    modelo.addRow(datos1);
+                    i++;
+	        }while(registros.next());
+	    }else{
+	        System.out.println("No se encuentran clientes registrados.");
+	    }
+	} catch (SQLException ex) {
+	    System.out.println("Error al buscar el cliente: "+ex.getMessage());
+	}   
+    }
+    
+    public void insertarDatosPersona(int documento, String nombre, String apellido, String tel, String correo){
         
         try {
-            String cedula = documento;
+            int cedula = documento;
             String nombres = nombre;
             String apellidos = apellido;
-            String direccion = "null";
             String telefono = tel;
             String email = correo;
 
-            String consulta = "INSERT INTO personas (cedula, nombres, apellidos, direccion, telefono, email) VALUES ('"+cedula+"','"+nombres+"','"+apellidos+"','"+direccion+"','"+telefono+"','"+email+"')";
+            String consulta = "INSERT INTO persona (ID_persona, Nombre, Apellido, Telefono, Email) VALUES ('"+cedula+"','"+nombres+"','"+apellidos+"','"+telefono+"','"+email+"')";
             int resp_consulta = manipularDB.executeUpdate(consulta);
             if (resp_consulta==1) {
                 respuesta = true;
@@ -78,7 +121,74 @@ public class BaseDatos {
             System.out.println("No se pudo insertar");
         } 
     }
-    public void buscarDAtos(String documento, JTextField cajanombre, JTextField cajaapellidos, JTextField cajatel, JTextField cajaemail){
+    public void insertarDatosEncargo(String fechaI, String fechaF, String anexos, int preciof, int abonado, int saldoP,int documento){
+        
+        try { 
+            String fechaPedido = fechaI;
+            String fechaEntrega= fechaF;
+            String descripcion = anexos;
+            int precio = preciof;
+            int abono=abonado;
+            int saldoPendiente=saldoP;
+            String estado= "en espera";
+            int id_Persona=documento;
+
+            String consulta = "INSERT INTO encargo ( FechaPedido, FechaEntrega, Descripcion, Precio, Abono, SaldoPendiente, Estado, persona_Id, id_usuario) VALUES ('"+fechaPedido+"','"+fechaEntrega+"','"+descripcion+"','"+precio+"','"+abono+"','"+saldoPendiente+"','"+estado+"','"+id_Persona+"')";
+            int resp_consulta = manipularDB.executeUpdate(consulta);
+            if (resp_consulta==1) {
+                respuesta = true;
+            }
+        } catch (SQLException ex) {
+            System.out.println("--> Error Insert: " + ex.getMessage());
+        }
+        if (respuesta){
+            System.out.println("Insertado con exito");
+        }else{
+            System.out.println("No se pudo insertar");
+        } 
+    }
+    public void insertarDatosReservas(int id_persona, String fechaR, int zona, String horaR, String anexos, int preciof, int abonado, int saldoP){
+        
+        try {
+            int persona=id_persona;
+            String fechaReserva=fechaR;
+            int lugar=zona;
+            String hora=horaR;
+            String descrip=anexos;
+            int precio = preciof;
+            int abono=abonado;
+            int saldoPendiente=saldoP;
+
+            String consulta = "INSERT INTO reserva (persona_id, FechaReserva, Zona, horaReserva, Anexos, Precio, Abono, SaldoPendiente ) VALUES ('"+persona+"','"+fechaReserva+"','"+lugar+"','"+hora+"','"+descrip+"','"+precio+"','"+abono+"','"+saldoPendiente+"')";
+            int resp_consulta = manipularDB.executeUpdate(consulta);
+            if (resp_consulta==1) {
+                respuesta = true;
+            }
+        } catch (SQLException ex) {
+            System.out.println("--> Error Insert: " + ex.getMessage());
+        }
+        if (respuesta){
+            System.out.println("Insertado con exito");
+        }else{
+            System.out.println("No se pudo insertar");
+        } 
+    }
+    public void buscarDatosReserva(String documento, JTextField cajanombre, JTextField cajaapellidos, JTextField cajatel, JTextField cajaemail){
+        ResultSet registros = null;
+	try {
+	    String consulta = "SELECT * FROM personas WHERE cedula='"+documento+"' ";
+	    registros = manipularDB.executeQuery(consulta);
+	    registros.next();
+            cajanombre.setText(registros.getString("nombres"));
+            cajaapellidos.setText(registros.getString("Apellidos"));
+            cajatel.setText(registros.getString("telefono"));
+            cajaemail.setText(registros.getString("email"));
+	    
+	} catch (SQLException ex) {
+	    System.out.println("Error al buscar el cliente: "+ex.getMessage());
+	}   
+    }
+    public void buscarDatosEncargo(String documento, JTextField cajanombre, JTextField cajaapellidos, JTextField cajatel, JTextField cajaemail){
         ResultSet registros = null;
 	try {
 	    String consulta = "SELECT * FROM personas WHERE cedula='"+documento+"' ";
