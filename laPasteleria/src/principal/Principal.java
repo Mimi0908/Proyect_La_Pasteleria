a8e7ec6d840a3f29eb02c423ddc113393dbd0b53
Naranjo Morales <naranjo.fabio.a@gmail.com>
2024-04-20 19:02:10 -0500

    adelanto

--- laPasteleria/src/principal/Principal.java
+++ laPasteleria/src/principal/Principal.java
@@ -1,10 +1,11 @@
 
 package principal;
-
+import utils.BaseDatos;
 public class Principal {
 
     public static void main(String[] args) {
         Login ventana= new Login();
+         BaseDatos database=new BaseDatos();
     }
     
 }
